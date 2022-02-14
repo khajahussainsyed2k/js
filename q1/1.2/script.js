ce761e6cfbf416ae308bf760115c5816431ee850
khajahussainsyed2k <khajahussain.syed@zemosolabs.com>
2022-02-14 17:31:10 +0530

    first commit

---
+++ q1/1.2/script.js
@@ -0,0 +1,8 @@
+const form=document.getElementById('signup');
+form.addEventListener('submit',(event)=>{
+    var FName=document.getElementById('firstName').value;
+    var LName=document.getElementById('lastName').value;
+    console.log(FName.charAt(0)+LName.charAt(0));
+    alert(FName.charAt(0)+LName.charAt(0));
+    event.preventDefault();
+})
---
+++ q1/function.js
@@ -0,0 +1,6 @@
+function function1(value){
+    return 'hello '+value;
+}
+function function2(a,function1){
+    alert(function1(a));
+}
