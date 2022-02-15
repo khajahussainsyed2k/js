02f590b1c9877b3b07d23c8a58686eebb40e58ad
khajahussainsyed2k <khajahussain.syed@zemosolabs.com>
2022-02-15 19:16:12 +0530

    async

---
+++ async-await/script.js
@@ -0,0 +1,12 @@
+function myDisplayer(email) {
+    console.log("start");
+    console.log("Email id of the user id is: " + email);
+    console.log("end");}
+    
+
+  
+  async function getData(uld) {return "skc@gmail.com";}
+  
+  getData("skc").then(
+    function(value) {myDisplayer(value);}
+  );
